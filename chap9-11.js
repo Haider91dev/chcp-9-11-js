b7838f4c9de8cf8551bf86c40d15686fb0067ebc
Haider91dev <154570409+Haider91dev@users.noreply.github.com>
2024-01-06 07:30:44 +0530

    chap-9-11-js

---
+++ chap9-11.js
@@ -0,0 +1,230 @@
+
+
+// Q1 Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+//  var city =prompt("enter a city name");
+
+//  var city1 =city.toLocaleUpperCase()
+//  console.log(city1)
+ 
+//  if(city1 =="KARACHI")
+//  {
+//       alert("Welcome to city of lights");
+
+//  }
+//  else{
+//       alert("thanks for visiting");
+//  } 
+
+//   Q2   Write a program to take “gender” as input from user. If the
+//  user is male, give the message: Good Morning Sir. If the
+//  user is female, give the message: Good Morning Ma’am.
+
+//  var gender =prompt("Please enter your gender")
+//  var gender1 =gender.toLocaleUpperCase();
+
+//  if (gender1 =="MALE") {
+//       alert("Good maorning sir")
+      
+//  }
+//  else if (gender1 =="FEMALE") {
+//       alert("Good morning MA'am")
+      
+//  } else {
+//       alert("please enter valid key")
+
+      
+//  }
+
+  //Q3
+
+//  var color =prompt("Please enter your color");
+ //  var color1 =color.toLocaleUpperCase();
+
+ //   if (color1 =="RED") {
+ //       alert("you must stop");
+      
+//   } else if (color1 =="GREEN") {
+
+//       alert("you move now");
+      
+//   } else if (color1 =="YELLOW") {
+//       alert("you ready to move");
+
+      
+//   } else {
+//       alert("enter a valid key");
+
+      
+//   } 
+
+ //Q4 
+//  var fuel = +prompt("check your fuel ");
+//  if (fuel<0.25) 
+//  {
+//   alert("Please refill the fuel in your car")
+
+  
+//  } else if(fuel>0.25)
+//  {
+//   alert("enjoy your riding of jounary")
+  
+//  }
+
+ //Q5 
+//  a  aler()
+//  b  no alert
+//  c no alert
+//   c ka a no alert
+//   c ka b alert
+//   c ka c no alert
+//   c ka d alert
+
+//   d alert
+//   e alert
+//   f alert
+
+
+//  //Q6
+//   var subA = +prompt("marks of a first subjet "); 
+//   var subB = +prompt("marks of a second subjet "); 
+//   var subC = +prompt("marks of a third  subjet "); 
+//   var total  = +prompt("total marks of subject "); 
+//   var percentage = ((subA+subB+subC)*100)/total;
+//   var obtained = subA+subB+subC;
+
+//   if (percentage>=80) {
+//     alert("Total marks : " +total+"\n" +
+//            "Marks obtained : " +obtained+"\n"
+//           + "Percentage  : " +percentage+"\n"
+//           + "Grade A-one"+"\n"
+//           + "Remarks Excellance " +"\n"
+//     )
+//   } else if  (percentage>=70 && percentage <=80)
+//   {
+//     alert("Total marks : " +total+"\n" +
+//            "Marks obtained : " +obtained+"\n"
+//           + "Percentage  : " +percentage+"\n"
+//           + "Grade A"+"\n"
+//           + "Remarks Good " +"\n"
+//     )
+//   }
+   
+//   else if (percentage >=60 && percentage<=70)
+//   {
+//     alert("Total marks : " +total+"\n" +
+//            "Marks obtained : " +obtained+"\n"
+//           + "Percentage  : " +percentage+"\n"
+//           + "Grade B"+"\n"
+//           + "Remarks You need to improve " +"\n"
+//     )
+//   }
+
+//   else if (percentage <60)
+//   {
+//     alert("Total marks : " +total+"\n" +
+//            "Marks obtained : " +obtained+"\n"
+//           + "Percentage  : " +percentage+"\n"
+//           + "Grade Fail"+"\n"
+//           + "Remarks Sorry " +"\n"
+//     )
+//   }
+
+//   else{
+//     alert("please enter valid marks")
+//   }
+
+ //Q8 
+//  var num =prompt("enter a number");
+ 
+//  if (num%3==0 ) {
+//   alert("bingo number is divisible by 3")
+//  } else 
+//  {
+//   alert("not divisible by 3")
+  
+//  }
+
+ //Q9
+//  var num =prompt("enter a number");
+//  if (num%2 ==0) {
+//   alert("number is even")
+
+//  } else {
+  // alert("number is odd")
+  
+//  }
+
+//10
+
+//  var num =prompt("enter a number");
+//  if (num>40) {
+//   alert("it is too hot outside")
+   
+//  } else if(num>30 && num<40)
+//  {
+//   alert("the weather today is normal")
+  
+//  }else if (num>20 && num<30)
+//  {
+//   alert("today weather is cool")
+
+//  }
+//  else if(num >10 && num<20)
+//  {
+//   alert("OMG ! today weather is so cool")
+
+//  }
+//  else{
+//   alert("error type a valid tempture ")
+
+//  }
+
+ //Q11
+
+//  var num1 = +prompt("enter a   first number");
+//  var num2 = +prompt("enter a   second  number");
+//  var operation = prompt("enter a operation");
+
+
+//  if (operation ==="+") {
+//   sum = num1+num2
+//   alert("sum of two number is  " + sum)
+  
+  
+//  } else if(operation ==="-") {
+//    alert("difference of two number"+num1-num2)
+//  }
+//  else if(operation ==="*")
+//  {
+//   alert("multipliction of two number "+num1*num2)
+//  }
+
+//  else if(operation ==="/")
+ 
+//  {
+
+//    alert("division of two number "+num1/num2);
+//  }
+
+//  else if(operation ==="%")
+//  {
+//   alert("modulus of two number "+num1%num2)
+//  }
+
+//  else{
+
+//    alert("enter valid operation");
+//  }
+  
+
+ 
+
+  
+
+
+
+
+  
